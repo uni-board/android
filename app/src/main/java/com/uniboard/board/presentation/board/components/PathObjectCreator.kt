ef8c5a91194fbe76d1f4e95b6e221bd6ced10ea9
renattele <renattele2018@gmail.com>
2024-07-03 12:52:31 +0300

    Fixed issue with PathCreator starting in 0,0

--- app/src/main/java/com/uniboard/board/presentation/board/components/PathObjectCreator.kt
+++ app/src/main/java/com/uniboard/board/presentation/board/components/PathObjectCreator.kt
@@ -6,9 +6,6 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -17,6 +14,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathSegment
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.copy
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.PointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
@@ -27,17 +25,13 @@ import com.uniboard.board.domain.RemoteObject
 import com.uniboard.board.domain.UObject
 import com.uniboard.board.presentation.board.BoardToolMode
 import com.uniboard.board.presentation.board.util.asCSSString
+import com.uniboard.core.presentation.rememberState
 import com.uniboard.core.presentation.theme.UniboardTheme
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonPrimitive
 import org.http4k.format.KotlinxSerialization.asJsonArray
 import org.http4k.format.KotlinxSerialization.asJsonValue
-
-private const val ACTION_IDLE = 0
-private const val ACTION_DOWN = 1
-private const val ACTION_MOVE = 2
-private const val ACTION_UP = 3
 
 @Composable
 fun PathObjectCreator(
@@ -47,16 +41,13 @@ fun PathObjectCreator(
 ) {
     val color = mode.color ?: Color.Black
     val width = mode.width ?: 5f
-    val path = remember { Path() }
-    var motionEvent by remember { mutableIntStateOf(ACTION_IDLE) }
-    var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
-
+    var path by rememberState { Path() }
     val drawModifier = modifier
         .pointerInput(Unit) {
             awaitEachGesture {
                 awaitFirstDown().also {
-                    motionEvent = ACTION_DOWN
-                    currentPosition = it.position
+                    path.moveTo(it.position.x, it.position.y)
+                    path = path.copy()
                 }
                 do {
                     val event: PointerEvent = awaitPointerEvent()
@@ -64,42 +55,17 @@ fun PathObjectCreator(
                         .forEachIndexed { _, pointerInputChange ->
                             if (pointerInputChange.positionChange() != Offset.Zero) pointerInputChange.consume()
                         }
-                    motionEvent = ACTION_MOVE
-                    currentPosition = event.changes.first().position
+                    val position = event.changes.first().position
+                    path.lineTo(position.x, position.y)
+                    path = path.copy()
                 } while (event.changes.any { it.pressed })
 
-                motionEvent = ACTION_UP
+                onCreate(path.createUObject(color, width))
             }
         }
-
-
-
 
     Canvas(modifier = drawModifier) {
-        println("CURRENT=$currentPosition")
-        when (motionEvent) {
-            ACTION_DOWN -> {
-                println("MOVE $currentPosition")
-                path.moveTo(currentPosition.x, currentPosition.y)
-            }
-
-            ACTION_MOVE -> {
-                if (currentPosition != Offset.Unspecified) {
-                    println("LINE $currentPosition")
-                    path.lineTo(currentPosition.x, currentPosition.y)
-                }
-            }
-
-            ACTION_UP -> {
-                println("CREATE $currentPosition")
-                path.lineTo(currentPosition.x, currentPosition.y)
-                onCreate(path.createUObject(color, width))
-                motionEvent = ACTION_IDLE
-            }
-
-            else -> Unit
-        }
-
+        println(path)
         drawPath(
             color = color,
             path = path,
